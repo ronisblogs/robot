2cf41115df8a48ef159b6f5dff5c03f3e6f9be5b
weifeng <weifeng@iscas.ac.cn>
2016-09-04 15:29:57 +0800

    添加了python的simplehttpserver作为简单服务器 #1 #4

--- cuber/scripts/locked.js
+++ cuber/scripts/locked.js
@@ -1,8 +1,11 @@
+/**
+*
+*
+*/
+
+
 ERNO.Locked = function ( cube, camera, domElement ) {
-
-
 	cube.domElement.ondragstart = function(){ return false };
-
 
 	var api = {
 		enabled : true,
@@ -10,9 +13,7 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 		rotateOnClick:false
 	};
 
-
 	var projector = new ERNO.Projector( cube, domElement ),
-		
 		axis = new THREE.Vector3(),
 		current = new THREE.Vector3(),
 		start = new THREE.Vector3(),
@@ -23,13 +24,10 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 		pixelRatio = window.devicePixelRatio || 1;
 		axisDefined = false;
 
-
 	var initialRotation = new THREE.Matrix4().makeRotationFromEuler( new THREE.Euler( Math.PI * 0.1, Math.PI * -0.25, 0 ));
-
 
 	// Returns the bounding area of the element
 	function getBoundingClientRect( element ){
-		
 		var bounds = element !== document ? element.getBoundingClientRect() : {
 			left: 0,
 			top: 0,
@@ -38,14 +36,12 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 		};
 
 	 	return bounds;
-
 	}
 
-
 	function onInteractStart( event ){
+		if(0!=event.button) return
 
 		if ( api.enabled && projector.getIntersection( camera, ( event.touches && event.touches[0] || event ).pageX, ( event.touches && event.touches[0] || event ).pageY ) === null ){
-
 			screen = getBoundingClientRect( domElement );
 
 			var x = ( event.touches && event.touches[0] || event ).pageX - screen.left;
@@ -54,23 +50,15 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 			x *= pixelRatio;
 			y *= pixelRatio;
 
-
 			// If the current coordinates are outside the cube,
 			// then this is a candidate for a full cube rotation. 
-
 			if( projector.getIntersection( camera, x, y ) === null ){
-
-
 				if( cube.isTweening() === 0 ){
-
-
 					// Get a time stamp
 					time = ( typeof window !== 'undefined' && window.performance !== undefined && window.performance.now !== undefined ? window.performance.now() : Date.now() )
 
-
 					start.set( x, y, 0 );
 					current.set( x, y, 0 );
-
 
 					domElement.removeEventListener( 'mousedown', onInteractStart );
 					document.addEventListener( 'mousemove', 	 onInteractMove );
@@ -79,22 +67,13 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 					domElement.removeEventListener( 'touchstart',onInteractStart );
 					document.addEventListener( 'touchmove', 	 onInteractMove );
 					document.addEventListener( 'touchend', 		 onInteractEnd );
-
-
 				}
-
 			}
-
 		}
-
 	}
 
-
 	function onInteractMove( event ){
-
-		
 		if( api.enabled ){
-
 			event.preventDefault();
 
 			var x = ( event.touches && event.touches[0] || event ).pageX - screen.left;
@@ -104,15 +83,11 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 			y *= pixelRatio;
 
 			current.set( x, y, 0 );
-
-
 		}
 	}
 
 
 	function onInteractEnd( event ){
-
-
 		domElement.addEventListener( 'mousedown', 	onInteractStart );
 		document.removeEventListener( 'mousemove',	onInteractMove );
 		document.removeEventListener( 'mouseup',  	onInteractEnd );
@@ -121,47 +96,32 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 		document.removeEventListener( 'touchmove', 	onInteractMove );
 		document.removeEventListener( 'touchend',  	onInteractEnd );
 
-
 		if( axisDefined ){
-
 			var command,
 				velocity,
 				angle;
 
-
 			// We have a group, but we need an associated command expressed as a single character.
-
 			if     ( group === cube.slicesDictionary['x'] ) command = 'x';
 			else if( group === cube.slicesDictionary['y'] ) command = 'y';
 			else if( group === cube.slicesDictionary['z'] ) command = 'z';
 
-
-
 			// Find the nearest 'complete' rotation
 			angle = -Math.round( group.rotation / Math.PI * 2.0 ) * Math.PI * 0.5;
 
-
-
 			// Get the velocity of the gesture.
-			velocity = direction.length() / (( typeof window !== 'undefined' && window.performance !== undefined && window.performance.now !== undefined ? window.performance.now() : Date.now() ) - time )
-			
+			velocity = direction.length() / (( typeof window !== 'undefined' && window.performance !== undefined && window.performance.now !== undefined ? window.performance.now() : Date.now() ) - time )	
 
 			// If the gesture is faster than a predefined speed, then we assume this
 			// is a swipe rather than a drag
 			
 			if( velocity > 0.8 ){
-
-
 				// If we are performing a 'z' twist, we should invert the rotation calculation
 				if( command === 'z' ) absDirection.negate();
 
-
 				// Depending on the gesture direction we'll need to set the rotation to positive or negative
-				
 				angle = Math.round( absDirection.dot( direction.normalize() )) * Math.PI * 0.5 * sign;
-				
 			}
-
 
 			// We'll need to bound the rotation to 90 degree intervals to ensure
 		 	// var origin = Math.round( group.rotation / Math.PI * 2.0 ) * Math.PI * 0.5;
@@ -169,10 +129,7 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 
 			// Now that we have defined a twist, add it to the stack
 			cube.twist( new ERNO.Twist( command, angle.radiansToDegrees() ))
-
 		}else{
-			
-
 			var command;
 
 			var x = ( event.touches && event.touches[0] || event ).pageX - screen.left;
@@ -193,7 +150,6 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 			axis.transformDirection( initialRotation );
 			axis.transformDirection( inverse );
 
-
 			if     ( Math.abs( Math.round( axis.x )) === 1 ) command = 'z';
 			else if( Math.abs( Math.round( axis.y )) === 1 ) command = 'y';
 			else if( Math.abs( Math.round( axis.z )) === 1 ) command = 'x';
@@ -209,8 +165,6 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 		current.set( 0, 0, 0 );
 		start.set( 0, 0, 0 );
 		time = 0;
-
-
 	}
 
 
@@ -218,16 +172,13 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 	//	This is usefult to to determine a probable axis of rotation. It's fairly naive, but it works.
 
 	function snapVectorToBasis( vector ){
-
-
 		var max = Math.max( Math.abs( vector.x ), Math.abs( vector.y ), Math.abs( vector.z ));
 
 		vector.x = Math.round( vector.x / max );
 		vector.y = vector.x === 1 ? 0 : ( vector.y / max )|0;
 		vector.z = vector.x === 1 || vector.y === 1 ? 0 : ( vector.z / max )|0; 
 
-		return vector;
-		
+		return vector;	
 	}
 
 
@@ -236,8 +187,6 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 	// returns the face most likely to be associated with it.
 
 	var getFace = function(){
-
-
 		var intersection = new THREE.Vector3(),
 			matrixInverse = new THREE.Matrix4(),
 			plane = new THREE.Plane();
@@ -245,19 +194,15 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 		var point = new THREE.Vector3( 0, 0, 0 );
 		
 		return function ( faces, x, y ){
-
 			var i = faces.length,
 				pointOfInteraction;
 			
 			cube.object3D.updateMatrixWorld();
 			matrixInverse.getInverse( cube.matrixWorld );
 
-
 			// For every face ...
 			while( i-- ){
-
 				point.set( x *-1 , y, 0 );
-
 
 				// get the associated plane ...
 				plane.normal.copy( faces[i].axis );
@@ -266,42 +211,28 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 				plane.normal.z = Math.abs( plane.normal.z );
 				plane.constant = cube.size * 0.5;
 				
-				plane.normal.transformDirection( initialRotation );
-				
+				plane.normal.transformDirection( initialRotation );			
 				
 				// and calculate where it intersects with the coordinates
 				plane.orthoPoint( point, intersection );
-
 
 				if( pointOfInteraction === undefined || intersection.z <= pointOfInteraction ){
 					pointOfInteraction = intersection.z;
 					face = faces[i];
 				}
-
 			}
 
 			return face;
-
 		}
 	}()
-
-
-	
-
-
 
 	domElement.addEventListener( 'mousedown', 	onInteractStart );
 	domElement.addEventListener( 'touchstart',  onInteractStart );
 
-
-	
 	api.update = function(){
-
-
 		// get a direction of movment
 		direction.x = current.x - start.x;
 		direction.y = current.y - start.y;
-
 
 		// If we have not previously defined an axis of rotation, 
 		// for example, when a user begins interacting and the movement is not accidental,
@@ -309,11 +240,9 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 
 		if( !axisDefined && direction.length() > 30 /*&& ( start.x !== current.x || start.y !== current.y )*/){
 
-
 			axisDefined = true;
 
 			sign = 1;
-
 
 			// Get an absolute directon, we use this to find signed rotation
 			absDirection.copy( direction );
@@ -324,23 +253,18 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 			absDirection.y = Math.abs( absDirection.y );
 			absDirection.z = Math.abs( absDirection.z );
 
-
 			// Get a vector perpendicular to the direction
 			axis.set( absDirection.y * -1, absDirection.x, 0 );
 			axis.normalize();
-
 
 			// Snap to the nearest basis
 
 			axis.x = Math.round( axis.x )
 			axis.y = Math.round( axis.y ) * ( 1.0 - Math.abs( axis.x ) );
-			axis.negate();
-			
+			axis.negate();			
 
 			// If the axis is horizontal, then we could be rotating on one of two axes
-			if( axis.y === 0 ){
-
-				
+			if( axis.y === 0 ){				
 				var face = getFace( [cube.front, cube.right ],
 				    current.x - ( screen.width * pixelRatio * 0.5 ),
 			    	current.y - ( screen.width * pixelRatio * 0.5 ) );
@@ -349,13 +273,9 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 				inverse.getInverse( cube.matrixWorld );
 				axis.transformDirection( initialRotation );
 				axis.transformDirection( inverse );
-
 				
 				sign = -1 * ( Math.round( axis.x ) ||  Math.round( axis.y ) ||  Math.round( axis.z ));
-				
-
-			}
-			
+			}			
 
 			// Find out the associated cube group to rotate based on the axis of rotation.
 			// ERNO.Cube maintains 3 special groups (X, Y, Z ) that contain all cubelets,
@@ -363,29 +283,19 @@ ERNO.Locked = function ( cube, camera, domElement ) {
 
 			if     ( Math.abs( Math.round( axis.x )) === 1 ) group = cube.slicesDictionary[ 'z' ];
 			else if( Math.abs( Math.round( axis.y )) === 1 ) group = cube.slicesDictionary[ 'y' ];
-			else if( Math.abs( Math.round( axis.z )) === 1 ) group = cube.slicesDictionary[ 'x' ];
-		
-
-			
+			else if( Math.abs( Math.round( axis.z )) === 1 ) group = cube.slicesDictionary[ 'x' ];			
 		}
-
-		
+	
 		// If we have an axis to rotate on, then we can calculate how much to rotate by
 
 		if( axisDefined ){
-
 			angle = -( absDirection.dot( direction ) / cube.size ) ;
 			if( group === cube.slicesDictionary[ 'z' ]  ) angle *= -1;
 			angle *= sign;
 			// group.rotation = Math.min( Math.PI * 0.5, Math.max( Math.PI * -0.5, angle * api.rotationSpeed ));
 			group.rotation = angle * api.rotationSpeed;
-
 		}
-
-
-
 	}
 
 	return api;
-
 }
--- cuber/scripts/main.js
+++ cuber/scripts/main.js
@@ -61,17 +61,65 @@
 	http://en.wikibooks.org/wiki/Template:Rubik's_cube_notation
 */
 
-$(document).ready( function(){ 
+$(document).ready( function(){
 	var useLockedControls = true,
 		controls = useLockedControls ? ERNO.Locked : ERNO.Freeform;
 
 	var ua = navigator.userAgent,
 		isIe = ua.indexOf('MSIE') > -1 || ua.indexOf('Trident/') > -1;
 
-	window.cube = new ERNO.Cube({
+	var W, O, B, R, G, Y, COLOURLESS;
+	W = ERNO.WHITE = new ERNO.Color("white", "W", "#FFF", "font-weight: bold; color: #888", "background-color: #F3F3F3; color: rgba( 0, 0, 0, 0.5 )");
+	O = ERNO.ORANGE = new ERNO.Color("orange", "O", "#F60", "font-weight: bold; color: #F60", "background-color: #F60; color: rgba( 255, 255, 255, 0.9 )");
+	B = ERNO.BLUE = new ERNO.Color("blue", "B", "#00D", "font-weight: bold; color: #00D", "background-color: #00D; color: rgba( 255, 255, 255, 0.9 )");
+	R = ERNO.RED = new ERNO.Color("red", "R", "#F00", "font-weight: bold; color: #F00", "background-color: #F00; color: rgba( 255, 255, 255, 0.9 )");
+	G = ERNO.GREEN = new ERNO.Color("green", "G", "#0A0", "font-weight: bold; color: #0A0", "background-color: #0A0; color: rgba( 255, 255, 255, 0.9 )");
+	Y = ERNO.YELLOW = new ERNO.Color("yellow", "Y", "#FE0", "font-weight: bold; color: #ED0", "background-color: #FE0; color: rgba( 0, 0, 0, 0.5 )");
+	ERNO.COLORLESS = new ERNO.Color("NA", "X", "#DDD", "color: #EEE", "color: #DDD");
+
+	//colorsFront = [Y,G,B,R,Y,W,G,R,R,G,Y,Y,O,O,W,R,R,O,O,W,Y,W,B,O,G,Y,B]
+	//colorsBack = [G,B,G,Y,R,B,O,W,B,W,O,G,B,O,Y,O,B,R,W,R,R,B,G,W,G,Y,W]
+	//colorsFront = [R,R,R,G,G,G,R,R,R,G,G,G,R,R,R,G,G,G,Y,Y,Y,Y,Y,Y,Y,Y,Y]
+	//colorsBack = [O,O,O,B,B,B,O,O,O,B,B,B,O,O,O,B,B,B,W,W,W,W,W,W,W,W,W]
+	colorsFront = [W,W,W,G,G,G,W,W,W,G,G,G,W,W,W,G,G,G,R,R,R,R,R,R,R,R,R]
+	colorsBack = [Y,Y,Y,B,B,B,Y,Y,Y,B,B,B,Y,Y,Y,B,B,B,O,O,O,O,O,O,O,O,O]
+
+	cubelets = [
+		//	front, up, right, down, left, back
+		[colorsFront[0], colorsFront[18], , , colorsBack[12]],		// 1
+		[colorsFront[1], colorsFront[19], , , ,],					// 2
+		[colorsFront[2], colorsFront[20], colorsFront[3], , ,],		// 3 
+		[colorsFront[6], , , ,colorsBack[6] ,],						// 4
+		[colorsFront[7], , , , ,],									// 5
+		[colorsFront[8], , colorsFront[9], , ,],					// 6
+		[colorsFront[12], , , colorsBack[18], colorsBack[0]],		// 7
+		[colorsFront[13], , , colorsBack[21], , ],					// 8
+		[colorsFront[14], , colorsFront[15], colorsBack[24], , ],	// 9
+		[, colorsFront[21], , , colorsBack[13]],					// 10
+		[,colorsFront[22], , , ,],									// 11
+		[, colorsFront[23], colorsFront[4], , , ],					// 12
+		[, , , , colorsBack[7]],									// 13
+		[, , , , , ], 												// 14
+		[, , colorsFront[10], , , ], 								// 15
+		[, , ,colorsBack[19] , colorsBack[1]],						// 16
+		[, , , colorsBack[22], , ],									// 17
+		[, , colorsFront[16], colorsBack[25], , ],					// 18
+		[, colorsFront[24], , , colorsBack[14], colorsBack[15]],	// 19
+		[, colorsFront[25], , , , colorsBack[16]],					// 20
+		[, colorsFront[26], colorsFront[5], , , colorsBack[17]],	// 21
+		[, , , , colorsBack[8], colorsBack[9]],						// 22
+		[, , , , , colorsBack[10]],									// 23
+		[, , colorsFront[11], , , colorsBack[11]],					// 24
+		[, , , colorsBack[20], colorsBack[2], colorsBack[3]],		// 25
+		[, , , colorsBack[23], , colorsBack[4]],					// 26
+		[, , colorsFront[17], colorsBack[26], , colorsBack[5]]		// 27
+	];
+
+	cube = new ERNO.Cube({
 		hideInvisibleFaces: true,
 		controls: controls,
-		renderer: isIe ? ERNO.renderers.IeCSS3D : null
+		renderer: isIe ? ERNO.renderers.IeCSS3D : null,
+		cubelets: cubelets
 	});
 
 	var container = document.getElementById( 'container' );
@@ -96,5 +144,4 @@ $(document).ready( function(){
 	// motion.range.y = Math.PI * 0.06;
 	// motion.range.z = 0;
 	// motion.paused = false;				// disables the effect
-
 })
