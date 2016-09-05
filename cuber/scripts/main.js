ab05421ce17e5a4b6377814f8cd23f3e4d30ead0
weifeng <weifeng@iscas.ac.cn>
2016-09-05 15:23:38 +0800

    使用jquery ajax的get方法从远端服务器获取数据，以便动态显示魔方.

--- cuber/scripts/main.js
+++ cuber/scripts/main.js
@@ -3,8 +3,29 @@
 */
 
 $(document).ready( function(){
-	server = new CuberServer("YGBRYWGRRGYYOOWRROOWYWBOGYB","GBGYRBOWBWOGBOYOBRWRRBGWGYW");
-	server.twist('b');
-	server.twist('f');
-	server.twist('l');
-})
+	//server = new CuberServer("YGBRYWGRRGYYOOWRROOWYWBOGYB","GBGYRBOWBWOGBOYOBRWRRBGWGYW");
+	//server.twist('b');
+	//server.twist('f');
+	//server.twist('l');
+	server = null
+	function loadCmd() {
+		 $.get("cmd",function(data,status){
+			if ("success"==status)
+			{
+				/*console.log(data)
+				if(54==data.length){
+					colors1=data.substring(0,27);
+					colors2=data.substring(27);
+				
+					server = new CuberServer(colors1,colors2);
+				}
+				else if (1==data.length)
+				{
+					server.twist(data);
+				}*/
+			}
+		});
+	}
+
+	setTimeout(loadCmd,100); 
+});
